88445cfae4957cb48672bb8c25d74c8c5ba01e3b
Dayeon <aakk9350@naver.com>
2020-06-24 23:57:12 +0900

    [클래스와 객체] Order.java 수정

--- Java_summer/src/base/Order.java
+++ Java_summer/src/base/Order.java
@@ -10,6 +10,11 @@ public class Order {
 	public String orderAddress;
 	
 	public void showInfo() {
-		System.out.println();
+		System.out.println("주문 번호: "+orderNumber);
+		System.out.println("주문자 아이디: "+personID);
+		System.out.println("주문 날짜: "+orderDate);
+		System.out.println("주문자 이름: "+personName);
+		System.out.println("주문 상품 번호: "+orderProductNumber);
+		System.out.println("배송 주소: "+orderAddress);
 	}
 }
