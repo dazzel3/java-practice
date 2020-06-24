b83b6e37e8e8821254cd3e284cf66d29518a3551
Dayeon <aakk9350@naver.com>
2020-06-24 23:31:06 +0900

    [클래스와 객체] 사람 정보 클래스 코딩

---
+++ Java_summer/src/base/Person.java
@@ -0,0 +1,18 @@
+package base;
+
+public class Person {
+	
+	public int age;
+	public String name;
+	public boolean isMarried;
+	public int numberOfChild;
+
+	public void showInfo() {
+		System.out.println("나이: "+age);
+		System.out.println("이름: "+name);
+		System.out.println("결혼여부: "+isMarried);
+		System.out.println("자녀수: "+numberOfChild);
+	}
+}
+	
+
