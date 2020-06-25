d6e49a64e84915d8a77a4e52331f2d1730abf6fb
Dayeon <aakk9350@naver.com>
2020-06-26 00:01:00 +0900

    [클래스와 객체] 정보 은닉 priavte 관련 코딩

---
+++ Java_summer/src/hiding/MyDateTest.java
@@ -0,0 +1,16 @@
+package hiding;
+
+public class MyDateTest {
+
+	public static void main(String[] args) {
+		
+		MyDate date = new MyDate();
+		
+		date.setDay(30);
+		date.setMonth(8);
+		date.setYear(2020);
+		
+		date.showDate();
+	}
+
+}
