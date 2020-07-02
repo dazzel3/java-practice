113d27fcab0be8a1be15e7e99bac93d941d16d1f
Dayeon <aakk9350@naver.com>
2020-07-02 12:21:34 +0900

    [다운캐스팅] 다운캐스팅과 instanceof 키워드 사용 코딩

--- Java_summer/src/polymorphism/AnimalTest.java
+++ Java_summer/src/polymorphism/AnimalTest.java
@@ -12,11 +12,19 @@ class Human extends Animal {
 	public void move() {
 		System.out.println("사람이 두발로 걷습니다.");
 	}
+	
+	public void readBooks() {
+		System.out.println("사람이 책을 읽습니다.");
+	}
 }
 
 class Tiger extends Animal {
 	public void move() {
 		System.out.println("호랑이가 네 발로 뜁니다.");
+	}
+	
+	public void hunting() {
+		System.out.println("호랑이가 사냥을 합니다.");
 	}
 }
 
@@ -24,6 +32,10 @@ class Tiger extends Animal {
 class Eagle extends Animal {
 	public void move() {
 		System.out.println("독수리가 하늘을 날아갑니다.");
+	}
+	
+	public void flying() {
+		System.out.println("독수리가 날개를 쭉 펴고 날아갑니다.");
 	}
 }
 
@@ -37,12 +49,7 @@ public class AnimalTest {
 		Animal tAnimal = new Tiger();
 		Animal eAnimal = new Eagle();
 		
-		AnimalTest test = new AnimalTest();
-		test.moveAniaml(hAnimal);
-		test.moveAniaml(tAnimal);
-		test.moveAniaml(eAnimal);
-		
-		System.out.println("========================");
+	
 		
 		ArrayList<Animal> animalList = new ArrayList<Animal>();
 		
@@ -54,6 +61,37 @@ public class AnimalTest {
 			animal.move();
 		}
 		
+		System.out.println("========================");
+		
+		AnimalTest test = new AnimalTest();
+		test.testDownCasting(animalList);
+		
+		
+		
+	}
+	
+	public void testDownCasting (ArrayList<Animal> list) {
+		for (int i=0; i<list.size(); i++) {
+			Animal animal = list.get(i);
+			
+			if (animal instanceof Human) {
+				Human human = (Human)animal;
+				human.readBooks();
+			}
+			else if (animal instanceof Tiger) {
+				Tiger tiger = (Tiger)animal;
+				tiger.hunting();
+			}
+			else if (animal instanceof Eagle) {
+				Eagle eagle = (Eagle)animal;
+				eagle.flying();
+			}
+			else {
+				System.out.println("error!");
+			}
+			
+			
+		}
 	}
 	
 	public void moveAniaml (Animal animal) {
