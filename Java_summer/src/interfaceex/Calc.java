5c6352fcaf77019ecf1e63b2ff87450ef4ff3865
Dayeon <aakk9350@naver.com>
2020-07-08 13:37:39 +0900

    [인터페이스] 인터페이스 구현 예제

---
+++ Java_summer/src/interfaceex/Calc.java
@@ -0,0 +1,12 @@
+package interfaceex;
+
+public interface Calc {
+	
+	double PI = 3.14;
+	int ERROR = -999999;
+	
+	public int add (int num1, int num2);
+	public int substract (int num1, int num2);
+	public int times (int num1, int num2);
+	public int divide (int num1, int num2);
+}
---
+++ Java_summer/src/interfaceex/CalcTest.java
@@ -0,0 +1,29 @@
+package interfaceex;
+
+public class CalcTest {
+
+	public static void main(String[] args) {
+	
+		CompleteCalc calc1 = new CompleteCalc();
+		Calc calc2 = new CompleteCalc();
+		
+		int n1 = 10;
+		int n2 = 2;
+		
+		System.out.println(calc1.add(n1, n2));
+		System.out.println(calc1.substract(n1, n2));
+		System.out.println(calc1.times(n1, n2));
+		System.out.println(calc1.divide(n1, n2));
+		
+		calc1.showInfo();
+		
+		System.out.println(calc2.add(n1, n2));
+		System.out.println(calc2.substract(n1, n2));
+		System.out.println(calc2.times(n1, n2));
+		System.out.println(calc2.divide(n1, n2));
+				
+		//calc2.showInfo(); 할 수 없음 왜냐면 calc2는 Calc형으로 선언되었기 때문에 Calc안에 있는 메서드만 사용가능
+		
+	}
+
+}
