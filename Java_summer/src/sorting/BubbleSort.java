e2d3d126a8fb11716cc70f7afe080794e940b730
Dayeon <aakk9350@naver.com>
2020-07-10 01:22:17 +0900

    [인터페이스] 정렬 알고리즘 관련 코딩

---
+++ Java_summer/src/sorting/BubbleSort.java
@@ -0,0 +1,22 @@
+package sorting;
+
+public class BubbleSort implements Sort {
+
+	@Override
+	public void ascending(int[] arr) {
+		System.out.println("BubbleSort ascending");
+	}
+
+	@Override
+	public void descending(int[] arr) {
+		System.out.println("BubbleSort descending");
+	}
+
+	@Override
+	public void description() {
+		Sort.super.description();
+		System.out.println("BubbleSort ¿‘¥œ¥Ÿ.");
+	}
+	
+	
+}
