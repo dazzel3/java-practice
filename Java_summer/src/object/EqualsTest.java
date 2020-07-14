45690f7826e8bec704398c50ff146254530d1ff4
Dayeon <aakk9350@naver.com>
2020-07-15 01:09:23 +0900

    [Object] equals(), hashCode() 메서드 오버라이딩

--- Java_summer/src/object/EqualsTest.java
+++ Java_summer/src/object/EqualsTest.java
@@ -1,14 +1,47 @@
 package object;
 
+class Student {
+	int studentID;
+	String studentName;
+	
+	public Student(int studentID, String studentName) {
+		this.studentID = studentID;
+		this.studentName = studentName;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Student) {
+			Student std = (Student)obj;
+			return (this.studentID == std.studentID);
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return studentID;
+	}
+}
+
 public class EqualsTest {
 
 	public static void main(String[] args) {
 		
-		String str1 = new String("abc");
-		String str2 = new String("abc");
+		Student lee = new Student(100,"이상민");
+		Student lee2 = lee;
+		Student sang = new Student(100,"이지은");
 		
-		System.out.println(str1 == str2);
-		System.out.println(str1.equals(str2));
+		System.out.println(lee == sang);
+		System.out.println(lee.equals(sang));
+		
+		System.out.println(lee.hashCode());
+		System.out.println(lee2.hashCode());
+		System.out.println(sang.hashCode());
+		
+		System.out.println(System.identityHashCode(lee));
+		System.out.println(System.identityHashCode(lee2));
+		System.out.println(System.identityHashCode(sang));
 	}
 
 }
