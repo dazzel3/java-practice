0489fce52fb2e495dfa0aac1b22ae786246bf8df
Dayeon <aakk9350@naver.com>
2020-07-22 14:27:17 +0900

    [예외와 예외처리] try-catch-finally구문과 try-with-resources문 구현

---
+++ Java_summer/src/exception/AutoCloseTest.java
@@ -0,0 +1,17 @@
+package exception;
+
+public class AutoCloseTest {
+
+	public static void main(String[] args) {
+		
+		AutoCloseObj obj = new AutoCloseObj();
+		
+		try (obj) {
+			throw new Exception();
+			
+		} catch (Exception e){
+			System.out.println(e);
+		}
+	}
+
+}
