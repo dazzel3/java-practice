5da0c9b512f85bc265cfe1364aee221327d15452
Dayeon <aakk9350@naver.com>
2020-07-25 23:07:37 +0900

    [보조 스트림] Data 스트림 구현

---
+++ Java_summer/src/decorator/DataStreamTest.java
@@ -0,0 +1,33 @@
+package decorator;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class DataStreamTest {
+
+	public static void main(String[] args) {
+		
+		try(FileOutputStream fos = new FileOutputStream("data.txt");
+				DataOutputStream dos = new DataOutputStream(fos);
+				FileInputStream fis = new FileInputStream("data.txt");
+				DataInputStream dis = new DataInputStream(fis)) {
+			
+			dos.writeByte(100);//1byte
+			dos.write(100);//4byte
+			dos.writeChar('A');
+			dos.writeUTF("æ»≥Á«œººø‰");
+			
+			System.out.println(dis.readByte());
+			System.out.println(dis.read());
+			System.out.println(dis.readChar());
+			System.out.println(dis.readUTF());
+			
+		}catch (IOException e){
+			
+		}
+	}
+
+}
